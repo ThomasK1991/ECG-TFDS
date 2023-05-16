01116a7048b263411b8d702966c7b4f31c0cb985
NumericalMax <max.kapsecker@gmail.com>
2023-05-16 13:26:17 +0200

    icentia11k - template structure

---
+++ src/icentia11k/icentia11k_dataset_builder_test.py
@@ -0,0 +1,24 @@
+"""icentia11k dataset."""
+
+import tensorflow_datasets as tfds
+from . import icentia11k_dataset_builder
+
+
+class Icentia11kTest(tfds.testing.DatasetBuilderTestCase):
+  """Tests for icentia11k dataset."""
+  # TODO(icentia11k):
+  DATASET_CLASS = icentia11k_dataset_builder.Builder
+  SPLITS = {
+      'train': 3,  # Number of fake train example
+      'test': 1,  # Number of fake test example
+  }
+
+  # If you are calling `download/download_and_extract` with a dict, like:
+  #   dl_manager.download({'some_key': 'http://a.org/out.txt', ...})
+  # then the tests needs to provide the fake output paths relative to the
+  # fake data directory
+  # DL_EXTRACT_RESULT = {'some_key': 'output_file1.txt', ...}
+
+
+if __name__ == '__main__':
+  tfds.testing.test_main()
